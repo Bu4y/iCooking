30f1022d776337a27de9096a0e68482c90d50475
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-09-21 12:21:12 +0700

    detail

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -15,7 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
+  detail = {
+    name: 'กระเพราหมูสับ + ไข่ดาว',
+    image: 'http://www.jjdelivery.com/new/images/product/00062.jpg',
+    detail: 'กระเพราหมูสับ + ไข่ดาว อร๊อย อร่อย',
+    material: ['1. หมูสับ', '2. ไข่ 1 ฟอง'],
+    step: ['1. เทน้ำมัน', '1. ทอดไข่']
+  }
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // this.detail = this.navParams.data;
   }
 
   ionViewDidLoad() {
